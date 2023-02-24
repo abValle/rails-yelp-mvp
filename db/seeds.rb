382b5728e53664ef96bf3bd49a9497016abe7f51
Allan Valle <allanvalle@outlook.com>
2023-02-24 17:03:44 -0300

    Solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning the data base...'
+Restaurant.destroy_all
+puts 'Seeding...'
+5.times do
+  new_restaurant = Restaurant.create!(name: Faker::Restaurant.name,
+                                      address: Faker::Address.street_address,
+                                      category: Restaurant::CATEGORY.sample,
+                                      phone_number: Faker::PhoneNumber.phone_number_with_country_code)
+  puts "Created Restaurant #{new_restaurant.id}"
+  Review.create!(
+    rating: rand(0..5),
+    content: Faker::Restaurant.review,
+    restaurant: new_restaurant
+  )
+end
+puts "Data base populated"
